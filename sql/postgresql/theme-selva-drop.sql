dfc9214250c774edaf48bac1f8eeb42b010d7129
Emmanuelle Raffenne <eraffenne@gmail.com>
2006-03-03 13:53:27 +0000

    Fixing drop sequence

--- sql/postgresql/theme-selva-drop.sql
+++ sql/postgresql/theme-selva-drop.sql
@@ -1,6 +1,5 @@
 UPDATE apm_parameter_values SET attr_value = '/packages/dotlrn/www/dotlrn-master' WHERE parameter_id IN (SELECT parameter_id FROM apm_parameters WHERE parameter_name='DefaultMaster');
 UPDATE portals SET theme_id = (SELECT theme_id FROM portal_element_themes WHERE name LIKE '%sloan%');
-DELETE FROM portal_element_themes WHERE name = 'selva';
 
 create function inline_0 ()
 returns integer as '
@@ -8,6 +7,7 @@ declare
     theme                       portal_element_themes.theme_id%TYPE;
 begin
     SELECT theme_id INTO theme FROM portal_element_themes WHERE name=''selva'';
+	DELETE FROM portal_element_themes WHERE theme_id = theme;
     perform acs_object__delete(theme);
 
 return 0;
