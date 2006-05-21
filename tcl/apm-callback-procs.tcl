62f359c174f9f605a3fc98b908ddd90364bec7dc
Don Baccus <dhogaza@pacifier.com>
2006-05-21 20:05:36 +0000

    Forgot to cvs-add the callback procs :)

---
+++ tcl/apm-callback-procs.tcl
@@ -0,0 +1,33 @@
+ad_library {
+
+     Theme Selva Package APM callbacks library
+
+     Procedures that deal with installing.
+
+     @creation-date May 2006
+     @author  Don Baccus (dhogaza@pacifier.com)
+     @cvs-id $Id$
+
+}
+
+namespace eval theme_selva {}
+namespace eval theme_selva::apm {}
+
+ad_proc -public theme_selva::apm::after_upgrade {
+    {-from_version_name:required}
+    {-to_version_name:required}
+} {
+    apm_upgrade_logic \
+        -from_version_name $from_version_name \
+        -to_version_name $to_version_name \
+	-spec {  
+	    0.7d5 2.2.0b1 {     
+                apm_parameter_register \
+                    AdditionalNavbarTabs \
+                    "list of URL Names pairs which will make it into additional navbar tabs" \
+                    theme-selva \
+                    "/dotlrn/ #dotlrn.Home# /theme-selva/courses #dotlrn.Courses# /theme-selva/communities #dotlrn.Communities# /dotlrn/control-panel #dotlrn.control_panel#" \
+                    string 
+	}
+    }
+}
